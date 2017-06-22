5de1f4fd4544e7189bd7b719ca551d57adfb81b4
David Paz <dpaz1993@gmail.com>
2017-06-22 11:04:58 +0200

    Added support for vendor and documentation attributes

--- utils.go
+++ utils.go
@@ -2,6 +2,7 @@ package enry
 
 import (
 	"bytes"
+	"io/ioutil"
 	"path/filepath"
 	"strings"
 
@@ -23,6 +24,9 @@ var (
 	configurationLanguages = map[string]bool{
 		"XML": true, "JSON": true, "TOML": true, "YAML": true, "INI": true, "SQL": true,
 	}
+
+	gitAttributes      = map[string]bool{}
+	languageAttributes = map[string]string{}
 )
 
 // IsAuxiliaryLanguage returns whether or not lang is an auxiliary language.
@@ -45,12 +49,30 @@ func IsDotFile(path string) bool {
 
 // IsVendor returns whether or not path is a vendor path.
 func IsVendor(path string) bool {
+<<<<<<< HEAD
 	return data.VendorMatchers.Match(path)
+=======
+	val, ok := gitAttributes[path]
+	if ok {
+		return val
+	}
+
+	return vendorMatchers.Match(path)
+>>>>>>> 132a9bb... Added support for vendor and documentation attributes
 }
 
 // IsDocumentation returns whether or not path is a documentation path.
 func IsDocumentation(path string) bool {
+<<<<<<< HEAD
 	return data.DocumentationMatchers.Match(path)
+=======
+	val, ok := gitAttributes[path]
+	if ok {
+		return val
+	}
+
+	return documentationMatchers.Match(path)
+>>>>>>> 132a9bb... Added support for vendor and documentation attributes
 }
 
 const sniffLen = 8000
@@ -67,4 +89,44 @@ func IsBinary(data []byte) bool {
 	}
 
 	return true
+}
+
+func loadGitattributes() (map[string]string, error) {
+	gitAttributes := map[string]string{}
+	data, err := ioutil.ReadFile(".gitattributes")
+	if err != nil {
+		return nil, err
+	}
+
+	if data != nil {
+		tokens := strings.Fields(string(data))
+		for i := 0; i < len(tokens); i = i + 2 {
+			gitAttributes[tokens[i]] = tokens[i+1]
+		}
+	}
+
+	return gitAttributes, nil
+}
+
+func parseAttributes(attributes map[string]string) {
+	for key, val := range attributes {
+		switch {
+		case val == "enry-vendored" || val == "enry-documentation":
+			gitAttributes[key] = true
+		case val == "enry-vendored=false" || val == "enry-documentation=false":
+			gitAttributes[key] = false
+		case strings.Contains(val, "enry-language="):
+			tokens := strings.Split(val, "=")
+			if len(tokens) == 2 {
+				languageAttributes[key] = tokens[1]
+			}
+		}
+	}
+}
+
+func init() {
+	rawAttributes, err := loadGitattributes()
+	if err == nil && len(rawAttributes) > 0 {
+		parseAttributes(rawAttributes)
+	}
 }
