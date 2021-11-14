79a60165528fccf7f22599ac70feafc063ffc20c
Daniel Shir <danielshir@gmail.com>
2021-11-14 14:23:16 +0200

    commit

--- benchmarks/parse.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-cd benchmarks/output
-go run ../parser/main.go -outdir ../csv
-cd ../csv
-go run ../parser/main.go -distribution
--- benchmarks/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-benchmarks/run-benchmarks.sh
-make benchmarks-slow
-benchmarks/parse.sh
-benchmarks/plot-histogram.gp
